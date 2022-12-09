0747187e710d16e954efa2b952eeba2e0f21d763
Covaar <co.va.ard@gmail.com>
2022-12-09 19:54:08 +0000

    Audio Player

--- src/js/component/Playlist.js
+++ src/js/component/Playlist.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import "../../styles/index.css"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faVolumeLow, faVolumeHigh, faForwardFast, faBackwardFast } from '@fortawesome/free-solid-svg-icons'
 const Playlist = () => {
 
-    const [infoSongs, setInfoSongs] = useState(null);
-    const List = []
+    const [songs, setSongs] = useState(null)
+    const [currentSong, setCurrentSong] = useState();
+    const [playButton, setPlayButton] = useState(true);
+    let audioRef = useRef();
 
 
     useEffect(() => {
@@ -22,48 +25,104 @@ const Playlist = () => {
                 'Content-Type': 'application/json'
             }
         }
-
         try {
             const response = await fetch(url, options_get);
             const data = await response.json();
             console.log(data);
-            setInfoSongs(data);
+            setSongs(data);
 
         } catch (error) {
             console.log(error.message);
         }
     }
-    //   const actualizarLista = async(infoSongs) =>{
-    //        if (infoSongs !== null){
-    //         List.append( infoSongs.data/fx.json);
-    //         List.append( infoSongs.data/songs.json);
-    //         console.log(List);
-    //        }  
-    //     }
+    const play = (url, i) => {
+        setCurrentSong(i);
+        setPlayButton(false);
+        audioRef.current.src = "https://assets.breatheco.de/apis/sound/" + url;
+        audioRef.current.play();
+    }
+
+    const handleButtonPlay = () => {
+        setPlayButton(false);
+        audioRef.current.play();
+    }
+
+    const handleButtonPause = () => {
+        setPlayButton(true);
+        audioRef.current.pause();
+        console.log(playButton)
+    }
+
+    const handleButtonBackward = () => {
+        if (currentSong - 1 < 0) {
+            audioRef.current.src = "https://assets.breatheco.de/apis/sound/" + songs[songs.length - 1].url;
+            audioRef.current.play();
+            setCurrentSong(songs.length - 1)
+        }
+        else {
+            audioRef.current.src = "https://assets.breatheco.de/apis/sound/" + songs[currentSong - 1].url;
+            audioRef.current.play();
+            setCurrentSong(currentSong - 1)
+        }
+    }
+
+    const handleButtonForward = () => {
+        if (currentSong + 1 >= songs.length) {
+            audioRef.current.src = "https://assets.breatheco.de/apis/sound/" + songs[0].url;
+            audioRef.current.play();
+            setCurrentSong(0)
+        }
+        else {
+            audioRef.current.src = "https://assets.breatheco.de/apis/sound/" + songs[currentSong + 1].url;
+            audioRef.current.play();
+            setCurrentSong(currentSong + 1)
+        }
+    }
+
+    const volumeUp = () => {
+        audioRef.current.volume += 0.1;
+    };
+
+    const volumeDown = () => {
+        audioRef.current.volume -= 0.1;
+    };
+
     return (
 
         <>
-            <div className="container ">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h1 className="text-center fw-bold">Audio player</h1>
-                    </div>
-
-                    {!!infoSongs &&
-                        infoSongs.length > 0 ?
-                        <ul class="list-group">
-                            {
-                                infoSongs.map((song, index) => {
-                                    return (
-                                        <li class="list-group-item Playlist text-center fst-italic" key={index} onClick={() => play(song.url, index)}> {index + 1}.- {song.name}</li>
-                                    )
-                                })} </ul> :
-                        (
-                            <div className="col-md-12 text-center">
-                                Loading...
-                            </div>
-                        )
+            <div className="cont">
+                <div className="title">
+                    <h1 className="text-center fw-bold">Audio player</h1>
+                </div>
+                <audio ref={audioRef}>
+                    Your browser does not support the audio tag.
+                </audio>
+                {!!songs &&
+                    songs.length > 0 ?
+                    <ul className="list-group">
+                        {
+                            songs.map((song, index) => {
+                                return (
+                                    <li className="list-group-item Playlist text-center fst-italic" key={index} onClick={() => play(song.url, index)}> {index + 1}.- {song.name}</li>
+                                )
+                            })} </ul> :
+                    (
+                        <div className="col-md-12 text-center">
+                            Loading...
+                        </div>
+                    )
+                }
+                <div className='controls'>
+                    <button><FontAwesomeIcon icon={faBackwardFast} onClick={handleButtonBackward} /></button>
+                    {
+                        !playButton ?
+                            <button><FontAwesomeIcon icon={faPause} onClick={handleButtonPause} /></button>
+                            :
+                            <button><FontAwesomeIcon icon={faPlay} onClick={handleButtonPlay} /></button>
                     }
+                    <button><FontAwesomeIcon icon={faForwardFast} onClick={handleButtonForward} /></button>
+                    <button><FontAwesomeIcon icon={faVolumeLow} onClick={volumeDown} /></button>
+                    <button><FontAwesomeIcon icon={faVolumeHigh} onClick={volumeUp} /></button>
                 </div>
             </div>
         </>
